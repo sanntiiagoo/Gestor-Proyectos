efe36ef5b813b548c78e10f2c7948dd988a54d2a
MarlonPra <mpastranamoreno@gmail.com>
2024-08-23 17:21:39 -0500

    HICE UNA MODIFICACION EN MODELS PARA QUE FUNCIONE LA BASE DE DATOS

--- user/models.py
+++ user/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+#TEST HOLA
+
 class User(AbstractUser):
     picture = models.ImageField(default='profile_default.png', upload_to='users/')
     location = models.CharField(max_length=60, null= True, blank=True)
