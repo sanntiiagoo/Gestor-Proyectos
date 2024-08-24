8093b180096d7fec6b9a53e5f6a216b0b4f87b0d
santiagov2004 <santiagovargad2004@gmail.com>
2024-08-23 19:07:04 -0500

    Finalizacion de la base de datos sprint 1

--- user/models.py
+++ user/models.py
@@ -8,4 +8,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     picture = models.ImageField(default='profile_default.png', upload_to='users/')
     location = models.CharField(max_length=60, null= True, blank=True)
-    
+    number_phone = models.IntegerField(max_length=14, null= True, blank=True)
