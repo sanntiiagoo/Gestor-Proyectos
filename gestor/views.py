4c6e6b8baf10f05dbade1016b7117e602125d0e5
MarlonPra <mpastranamoreno@gmail.com>
2024-08-25 21:15:03 -0500

    Agregue la autenticacion del backend

--- gestor/views.py
+++ gestor/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from user.models import User
 from django.contrib import messages
-#from django.contrib.auth.hashers import make_password
+import re
 
 
 #----------------Inicio----------------
@@ -23,20 +23,53 @@ def actualizarperfil(request):
         number_phone = request.POST.get('number_phone')
         location = request.POST.get('location')
         password = request.POST.get('password')
+        current_password = request.POST.get('current_password')  # Campo para la contraseña actual
 
-        # Actualizar los campos del usuario
         user = request.user
-        user.username = username
-        user.email = email
-        user.number_phone = number_phone
-        user.location = location
+        if username != user.username or email != user.email or number_phone != user.number_phone or location != user.location or password:
+            # Validaciones
+            if username != user.username:
+                if not re.match(r'^[a-zA-Z]{8,50}$', username):
+                    messages.error(request, 'El nombre de usuario debe tener entre 8 y 50 caracteres y no puede contener espacios ni números.')
+                    return redirect('actualizar_perfil')
 
-        if password:
-            user.set_password(password)
+            if email != user.email:
+                if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+                    messages.error(request, 'Por favor, ingresa un correo electrónico válido.')
+                    return redirect('actualizar_perfil')
 
-        user.save()
+            if password and (len(password) < 7 or len(password) > 20):
+                messages.error(request, 'La contraseña debe tener entre 7 y 20 caracteres.')
+                return redirect('actualizar_perfil')
 
-        messages.success(request, 'Tu perfil ha sido actualizado exitosamente.')
-        return redirect('projectos')  # Redirige de vuelta a la página de perfil
+            if number_phone != user.number_phone:
+                if not re.match(r'^\d{2}\d{10}$', number_phone):
+                    messages.error(request, 'El número de teléfono debe tener 12 dígitos, incluyendo el código de país.')
+                    return redirect('actualizar_perfil')
+
+            if location != user.location:
+                if not re.match(r'^[a-zA-Z\s]{7,20}$', location):
+                    messages.error(request, 'El lugar de residencia debe tener entre 7 y 20 caracteres y no incluir caracteres especiales.')
+                    return redirect('actualizar_perfil')
+
+            # Verificación de contraseña actual antes de cambiarla
+            if password:
+                if not user.check_password(current_password):
+                    messages.error(request, 'La contraseña actual no es correcta.')
+                    return redirect('actualizar_perfil')
+                user.set_password(password)
+
+            # Actualizar los campos del usuario
+            user.username = username
+            user.email = email
+            user.number_phone = number_phone
+            user.location = location
+            user.save()
+
+            messages.success(request, 'Tu perfil ha sido actualizado exitosamente.')
+            return redirect('projectos')  # Redirige de vuelta a la página de perfil
+        else:
+            messages.info(request, 'No hubo ningun cambio en tu perfil.')
+            return redirect('projectos')  # Redirige de vuelta a la página de perfil
 
     return render(request, 'perfilconfig.html')  # Asegúrate de que este nombre coincida con tu archivo de plantilla
